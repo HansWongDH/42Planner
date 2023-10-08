7b7986cf8eb52631b8602e86092de121036019ea
Josh Thor <wunjosh@gmail.com>
2023-10-08 11:49:48 +0800

    chore: Removed API call from BlackholeCalendar
    Replaced the API call with data attached to the UserData interface.

--- frontend/src/app/components/bhd_calendar/BlackholeCalendar.tsx
+++ frontend/src/app/components/bhd_calendar/BlackholeCalendar.tsx
@@ -1,5 +1,3 @@
-import callAPI from "@/app/libs/CallApi";
-import { useAccessToken } from "@/app/libs/stores/useSessionStore";
 import { useCurrentUser } from "@/app/libs/stores/useSessionStore";
 import { useEffect, useState } from "react";
 import React from "react";
@@ -11,29 +9,20 @@ type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function DeathCalendar() {
-  const accessToken = useAccessToken();
   const currentUser = useCurrentUser();
   const [death_date, setDeathDate] = useState<string | null>(null);
   const [date, setDate] = useState<Value>([new Date(), new Date()]);
 
   useEffect(() => {
-    async function fetchDeathDay(accessToken: string) {
-      const data = await callAPI("GET", "me", accessToken);
-      setDeathDate(data.body.cursus_users[1].blackholed_at);
-      console.log(data.body);
-    }
     if (currentUser) {
-      fetchDeathDay(accessToken ?? "");
-      if (death_date) setDate([new Date(), new Date(death_date)]);
+      const temp = currentUser?.cursus_users[1].blackholed_at;
+      setDeathDate(temp);
     }
-  }, [currentUser, accessToken]);
-
-  useEffect(() => {
     if (death_date) {
+      console.log("AAAAAAAAAAAAAAAAAA");
       setDate([new Date(), new Date(death_date)]);
-      console.log(death_date);
     }
-  }, [death_date]);
+  }, [currentUser, death_date]);
 
   if (!death_date) return;
 
